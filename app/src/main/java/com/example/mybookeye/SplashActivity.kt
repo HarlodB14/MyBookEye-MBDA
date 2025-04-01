a80274681ea9635e4aaa56f30f6f9b1ea4a6ffaf
Harlod Bombala <h.bombala@student.avans.nl>
2025-04-01 18:02:11 +0200

    changes regarding splashacitivity, login/registerscreen, libraries
    - added more styles, libries regarding activities and layouts
    - added login/registerscreen
    - added splashactivity

---
+++ app/src/main/java/com/example/mybookeye/SplashActivity.kt
@@ -0,0 +1,34 @@
+package com.example.mybookeye
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.example.mybookeye.ui.theme.MyBookEyeTheme
+
+class SplashActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        installSplashScreen()
+
+        startActivity(Intent(this, MainActivity::class.java))
+        finish()
+    }
+}
